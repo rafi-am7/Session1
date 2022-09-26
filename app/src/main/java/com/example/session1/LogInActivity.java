bea9b1b543ca5baa251140c9914507b69f77a4c5
Rafi Al Mahmud <rafialmahmudc@gmail.com>
2022-09-27 00:19:58 +0600

    final push

--- app/src/main/java/com/example/session1/LogInActivity.java
+++ app/src/main/java/com/example/session1/LogInActivity.java
@@ -1,14 +1,31 @@
 package com.example.session1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LogInActivity extends AppCompatActivity {
+    EditText demoEditText;
+    TextView demoTextView;
+    Button demoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        demoEditText = findViewById(R.id.edittext);
+        demoTextView=findViewById(R.id.id1);
+        demoButton = findViewById(R.id.demoButton);
+        demoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = demoEditText.getText().toString();
+                demoTextView.setText(s);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/session1/MainActivity.java
+++ app/src/main/java/com/example/session1/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.session1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button takeMeButton;
+    TextView textView;
+   // int x=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        takeMeButton = findViewById(R.id.takeMeButton);
+        textView = findViewById(R.id.tvId);
+        takeMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent x = new Intent(MainActivity.this,LogInActivity.class);
+                startActivity(x);
+                finish();
+            }
+        });
     }
 }
